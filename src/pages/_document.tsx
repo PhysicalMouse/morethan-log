45d8dc4bbff098f0ecd3cf3e8c5713e255441b35
PhysicalMouse <45204810+PhysicalMouse@users.noreply.github.com>
2025-09-04 05:42:57 +0000

    h1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,8 +39,8 @@ class MyDocument extends Document {
           {/* baidu search advisor */}
           <meta name="baidu-site-verification" content="codeva-fN1CHA3fGo" />
         </Head>
-        <h1>PhysicalMouse's Personal Blog for Blender Tips and Artwork Showcase！</h1>
         <body>
+          <h1>PhysicalMouse's Personal Blog for Blender Tips and Artwork Showcase！</h1>
           <Main />
           <NextScript />
         </body>
